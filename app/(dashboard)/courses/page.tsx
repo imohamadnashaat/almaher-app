eede70c9612a5d585674e14c1ff443f86f802497
Mohamad Nashaat <imohamadnashaat@gmail.com>
2024-06-09 16:29:24 +0300

    Update courses page to display a list of courses

--- app/(dashboard)/courses/page.tsx
+++ app/(dashboard)/courses/page.tsx
@@ -5,14 +5,22 @@ import useSWR from 'swr';
 export default function Student() {
   const { data, error } = useSWR('https://app.almaher.one/api/courses/');
 
-  if (error) return <div>failed to load. {error.message}</div>;
-  if (!data) return <div>loading...</div>;
+  if (error) return <div>Failed to load. {error.message}</div>;
+  if (!data) return <div>Loading...</div>;
 
   // render data
   return (
     <>
       <h1>الدورات</h1>
-      <div>{data[0].courses_name}</div>
+      <div>
+        <ul>
+          {data.map((course) => (
+            <li key={course.course_id}>
+              <a href={`/courses/${course.course_id}`}>{course.course_name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
