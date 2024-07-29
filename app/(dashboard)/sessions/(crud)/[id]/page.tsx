9570b08bd6a62d56801b2ad8ecbd65ca9adf68e0
Mohamad Nashaat <imohamadnashaat@gmail.com>
2024-07-30 01:39:10 +0300

    Update student wait list functionality to session view page

--- app/(dashboard)/sessions/(crud)/[id]/page.tsx
+++ app/(dashboard)/sessions/(crud)/[id]/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 import {
   Session,
   SessionAvailableStudent,
   SessionStudentDetails,
 } from '../../../../lib/types';
 import Loading from '../../../../../components/Loading';
+import { postRequest } from '../../../../lib/api';
+import toast from 'react-hot-toast';
 
 export default function SessionView() {
   const selectedCourseId = localStorage.getItem('selectedCourseId');
@@ -29,6 +31,30 @@ export default function SessionView() {
   const { data: availableStudentsData, error: waitListError } = useSWR<
     SessionAvailableStudent[]
   >(`sessions/students/available/?course_id=${selectedCourseId}`);
+
+  const handleAddStudent = async (studentId: number) => {
+    const body = {
+      session_id: sessionData?.session_id,
+      student_id: studentId,
+    };
+
+    try {
+      const result = await postRequest('sessions/students/add/', body);
+      toast.success('تم تحديث البيانات بنجاح', {
+        duration: 2000,
+      });
+      // Update the students list, and the wait list
+      mutate(
+        `sessions/students/details/?course_id=${selectedCourseId}&session_id=${id}`
+      );
+      mutate(`sessions/students/available/?course_id=${selectedCourseId}`);
+    } catch (error) {
+      console.error(error);
+      toast.error('حدث خطأ أثناء تحديث البيانات', {
+        duration: 2000,
+      });
+    }
+  };
 
   if (sessionError || studentsError || waitListError)
     return <div className="text-red-500">Failed to load</div>;
@@ -123,7 +149,12 @@ export default function SessionView() {
               <td className="px-4 py-2">{'priority_id'}</td>
               <td className="px-4 py-2">{student.level_id_id}</td>
               <td className="px-4 py-2 text-start">
-                <button className="w-6 bg-green-500 rounded-2xl">➕</button>
+                <button
+                  className="w-6 bg-green-500 rounded-2xl"
+                  onClick={() => handleAddStudent(student.person_id)}
+                >
+                  ➕
+                </button>
               </td>
             </tr>
           ))}
