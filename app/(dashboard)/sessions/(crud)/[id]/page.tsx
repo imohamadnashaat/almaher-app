4e7b07808b88bb4ed6fbb89c9b97b1d854b521ed
Mohamad Nashaat <imohamadnashaat@gmail.com>
2024-07-30 01:23:02 +0300

    Add student wait list functionality to session view page

--- app/(dashboard)/sessions/(crud)/[id]/page.tsx
+++ app/(dashboard)/sessions/(crud)/[id]/page.tsx
@@ -2,7 +2,11 @@
 
 import { usePathname } from 'next/navigation';
 import useSWR from 'swr';
-import { Session, SessionStudentDetails } from '../../../../lib/types';
+import {
+  Session,
+  SessionAvailableStudent,
+  SessionStudentDetails,
+} from '../../../../lib/types';
 import Loading from '../../../../../components/Loading';
 
 export default function SessionView() {
@@ -21,10 +25,15 @@ export default function SessionView() {
   >(
     `sessions/students/details/?course_id=${selectedCourseId}&session_id=${id}`
   );
+  // Fetch students wait list
+  const { data: availableStudentsData, error: waitListError } = useSWR<
+    SessionAvailableStudent[]
+  >(`sessions/students/available/?course_id=${selectedCourseId}`);
 
-  if (sessionError || studentsError)
+  if (sessionError || studentsError || waitListError)
     return <div className="text-red-500">Failed to load</div>;
-  if (!sessionData || !studentsData) return <Loading />;
+  if (!sessionData || !studentsData || !availableStudentsData)
+    return <Loading />;
 
   return (
     <div className="container mx-auto p-4">
@@ -56,7 +65,9 @@ export default function SessionView() {
           </p>
         </div>
       </div>
-      <h2 className="text-xl font-semibold mb-4">الطلاب</h2>
+
+      {/* Session students table */}
+      <h2 className="text-xl font-semibold my-4">الطلاب</h2>
       <table className="min-w-full table-auto">
         <thead className="bg-gray-200">
           <tr>
@@ -66,6 +77,7 @@ export default function SessionView() {
             <th className="px-4 py-2 text-start">تاريم الميلاد</th>
             <th className="px-4 py-2 text-start">الأولوية</th>
             <th className="px-4 py-2 text-start">المستوى</th>
+            <th className="px-4 py-2 text-start">ازالة</th>
           </tr>
         </thead>
         <tbody className="bg-white">
@@ -77,6 +89,42 @@ export default function SessionView() {
               <td className="px-4 py-2">{student.student_data.bdate}</td>
               <td className="px-4 py-2">{student.student_data.priority_id}</td>
               <td className="px-4 py-2">{student.student_data.level_id}</td>
+              <td className="px-4 py-2">
+                <button>❌</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Student wait list table */}
+      <h2 className="text-xl font-semibold my-4">قائمة الانتظار</h2>
+      <table className="min-w-full table-auto">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="px-4 py-2 text-start">ID</th>
+            <th className="px-4 py-2 text-start">الاسم</th>
+            <th className="px-4 py-2 text-start">اسم الاب</th>
+            <th className="px-4 py-2 text-start">تاريم الميلاد</th>
+            <th className="px-4 py-2 text-start">الأولوية</th>
+            <th className="px-4 py-2 text-start">المستوى</th>
+            <th className="px-4 py-2 text-start">اضافة</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {availableStudentsData.map((student) => (
+            <tr key={student.person_id} className="border-b">
+              <td className="px-4 py-2">{student.person_id}</td>
+              <td className="px-4 py-2">
+                {student.first_name} {student.last_name}
+              </td>
+              <td className="px-4 py-2">{'father_name'}</td>
+              <td className="px-4 py-2">{student.bdate}</td>
+              <td className="px-4 py-2">{'priority_id'}</td>
+              <td className="px-4 py-2">{student.level_id_id}</td>
+              <td className="px-4 py-2 text-start">
+                <button className="w-6 bg-green-500 rounded-2xl">➕</button>
+              </td>
             </tr>
           ))}
         </tbody>
